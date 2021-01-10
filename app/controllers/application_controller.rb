fa538f3675e95476edacbc9642277a14161a7f08
nagata001 <manfree875@gmail.com>
2021-01-10 21:06:19 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # before_action :basic_auth
+
+  # private
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["production"] && password == ENV["aace360912f6719fe49102037abe13da"]
+  #   end
+  # end
 end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,5 @@
+class PostsController < ApplicationControllerdef 
+    index
+  end
+
+end
